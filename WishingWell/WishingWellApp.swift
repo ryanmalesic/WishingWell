e813fefe879b90fd66b4a6414132a9d1d9bdaba6
Ryan Malesic <ryanmalesic@icloud.com>
2020-08-21 15:08:18 -0400

    Initial Commit

---
+++ WishingWell/WishingWellApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishingWellApp.swift
+//  WishingWell
+//
+//  Created by Ryan Malesic on 8/21/20.
+//
+
+import SwiftUI
+
+@main
+struct WishingWellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
